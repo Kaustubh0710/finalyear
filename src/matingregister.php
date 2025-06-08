e1dc0f41c70e15157787fac45e1f70de7a6e9363
Kaustubh0710 <kaustubhbastawadi@gmail.com>
2025-06-09 03:02:47 +0530

    I added few pages & that login check part

--- src/matingregister.php
+++ src/matingregister.php
@@ -9,7 +9,7 @@ $dbname = "happytails";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+// Check connections
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
